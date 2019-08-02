39c376edac8518b18f8e3fee1b3c99f92c578266
Greg Quintana <quintana.greg444@gmail.com>
2019-08-01 20:14:25 -0600

    added ajax call and hooked api

---
+++ assets 8.22.28 PM/javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ var artist="cure"  
+ var queryURL ="https://rest.bandsintown.com/artists/"+artist+"?app_id="+apiKey;
+  console.log("working")
+ $.ajax({
+  method:"GET",
+  url:queryURL
+ }).then(function(responseData){
+ console.log(responseData);
+});
+
+});
